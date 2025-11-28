f056f45bd8eca5d634e85b86d20790c8d867a194
hThEextremGrummpy7 <ht13201010@gmail.com>
2025-11-28 14:34:17 +0800

    update single file

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
     const data = await request.json();
   
     const res = await fetch(
-      "https://hthegrumpy.app.n8n.cloud/webhook/2147b71b-bc5d-41ca-85ff-5ed4c2179767",
+      "https://hthegrumpy.app.n8n.cloud/webhook/312e7413-819d-4352-80a4-3b92fa8c5731",
       {
         method: "POST",
         headers: {
